68094f02f369e1768703dfc9d769bef9e612e024
Rafael <79052980+webheresy@users.noreply.github.com>
2024-06-24 20:55:07 -0300

    upei meu projeto

---
+++ acesso/index.js
@@ -0,0 +1,89 @@
+function show() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto1");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254847237398204606/foto1.jpg?ex=667afb0f&is=6679a98f&hm=4f1a83169c3722a4dd6f9362d73e0d0b4f41922f012d0176fd6a6d87ef80a634&"
+   document.getElementById("imagem1")
+       .style.display = "none";
+}
+
+function show2() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto2");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254847236974706791/foto2.jpg?ex=667afb0f&is=6679a98f&hm=399c3bff2b5272b519ebf0a633b617102f68eb6c40920aecdb59943b0f78b4c7&"
+   document.getElementById("imagem2")
+       .style.display = "none";
+}
+
+function show3() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto3");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254847237176168581/foto3.jpg?ex=667afb0f&is=6679a98f&hm=fd5dcdd060f890489573a24374520771247404270d3a36bfeae7d4c13fce6e0c&"
+   document.getElementById("imagem3")
+       .style.display = "none";
+}
+
+function show4() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto4");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254863617686372505/IMG-20240330-WA0022.jpg?ex=667b0a50&is=6679b8d0&hm=dff7ddf09e46c15c6414b4819386922d765093969ecd0f3f9dddee4060fc6bf8&"
+   document.getElementById("imagem4")
+       .style.display = "none";
+}
+
+function show5() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto5");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254863617422135396/IMG-20240514-WA0027.jpg?ex=667b0a50&is=6679b8d0&hm=cdcd370d45aab525bca9198344c31a9f2716bafb1e83120ec439ec658660b733&"
+   document.getElementById("imagem5")
+       .style.display = "none";
+}
+
+function show6() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto6");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254863617933967572/IMG-20240423-WA0060.jpg?ex=667b0a50&is=6679b8d0&hm=53dee963690bc68261388d5b0c8f2a0b48a51b60b0e1262fd2894b75def04ded&"
+   document.getElementById("imagem6")
+       .style.display = "none";
+}
+
+function show7() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto7");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254867433899036843/IMG-20240424-WA0041.jpg?ex=667b0dde&is=6679bc5e&hm=eb620709a57ff63e9c9871354fc7e0bbecf7c92802db2caa8cdad05687698631&"
+   document.getElementById("imagem7")
+       .style.display = "none";
+}
+
+function show8() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto8");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254867434305749142/IMG_20240424_172333_848.jpg?ex=667b0dde&is=6679bc5e&hm=b669fd6ba3e6e944d632a4fee89787cf50fd394a4bb3f9d89217f999b1022b5b&"
+   document.getElementById("imagem8")
+       .style.display = "none";
+}
+
+function show9() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto9");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254867434565799936/IMG_20240429_221848_977.jpg?ex=667b0dde&is=6679bc5e&hm=432dd9fe19df15a2838fea42fe3fa06b0ab47ded63ae2268acf98357f38de892&"
+   document.getElementById("imagem9")
+       .style.display = "none";
+}
+
+function show10() {
+   /* Get image and change value 
+   of src attribute */
+   let image = document.getElementById("foto10");
+   image.src = "https://cdn.discordapp.com/attachments/743824010227351575/1254867434867785831/IMG_20240607_220846_398.png?ex=667b0dde&is=6679bc5e&hm=b450852759a00525b214cb7cea33e962e267c7e9d01a5e58f8caf6b04f9bbdbe&"
+   document.getElementById("imagem10")
+       .style.display = "none";
+}
